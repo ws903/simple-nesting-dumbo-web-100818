fc9ef4b4ee67680d0690a5973cd23897494e2da3
David Sim <wsim903@gmail.com>
2018-09-29 20:53:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
         }
      }
   programmer_hash[:yukihiro_matsumoto] = {
-  :known_for => "Ruby",
+    :known_for => "Ruby",
     :languages => ["LISP", "C"]
-}
+  }
 end
 
 def changing_alan
